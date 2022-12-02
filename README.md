2b4750bfc2d83cb34eae010227b65d09b2373989
Simon <simon.abennet@spartans.ut.edu>
2022-12-01 20:55:55 -0500

    Completed Blockchain code and README.md

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# Blockchain-Python
-Blockchain-based ledger system with a user-friendly web interface
+## Overview of the Analysis: Blockchain-Python
+Building a blockcahin-based ledger system with a user-friendly web interface. This ledger can be used in many instances such as acting as a ledger that allow partner banks to conduct financial transactions, and to verify the integrity of the data. 
+
+
+## Technology
+Pandas, Streamlit, Dataclasses, Datetime, Hashlib, Blockchain
+
+
+## Results
+Screenshot 2022-12-01 at 6.57.07 PM.png
--- pychain.py
+++ pychain.py
@@ -49,7 +49,16 @@ import hashlib
 # @TODO
 # Create a Record Data Class that consists of the `sender`, `receiver`, and
 # `amount` attributes
-# YOUR CODE HERE
+@dataclass
+
+class Record:
+    
+    sender: str
+    
+    receiver: str
+    
+    amount: float
+    
 
 
 ################################################################################
@@ -68,8 +77,7 @@ class Block:
 
     # @TODO
     # Rename the `data` attribute to `record`, and set the data type to `Record`
-    data: Any
-
+    record: Record
     creator_id: int
     prev_hash: str = "0"
     timestamp: str = datetime.datetime.utcnow().strftime("%H:%M:%S")
@@ -164,21 +172,17 @@ pychain = setup()
 # 4. Add an input area where you can get a value for `amount` from the user.
 # 5. As part of the Add Block button functionality, update `new_block` so that `Block` consists of an attribute named `record`, which is set equal to a `Record` that contains the `sender`, `receiver`, and `amount` values. The updated `Block`should also include the attributes for `creator_id` and `prev_hash`.
 
-# @TODO:
-# Delete the `input_data` variable from the Streamlit interface.
-input_data = st.text_input("Block Data")
 
 # @TODO:
+input_data = st.text_input('Block')
 # Add an input area where you can get a value for `sender` from the user.
-# YOUR CODE HERE
-
+sender = st.text_input('Sender')
 # @TODO:
 # Add an input area where you can get a value for `receiver` from the user.
-# YOUR CODE HERE
-
+reciever = st.text_input ('Reciever')
 # @TODO:
 # Add an input area where you can get a value for `amount` from the user.
-# YOUR CODE HERE
+amount = st.text_input('Amount')
 
 if st.button("Add Block"):
     prev_block = pychain.chain[-1]
@@ -189,7 +193,7 @@ if st.button("Add Block"):
     # which is set equal to a `Record` that contains the `sender`, `receiver`,
     # and `amount` values
     new_block = Block(
-        data=input_data,
+        record=Record(sender,reciever,amount),
         creator_id=42,
         prev_hash=prev_block_hash
     )
